742d9995cc7ea82b6b46b1eccda97ef33f552a85
Eyasu Teshome <joshtesh91@mail.com>
2022-08-10 17:35:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
